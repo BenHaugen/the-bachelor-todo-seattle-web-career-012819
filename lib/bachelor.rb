11da9bed913b346ff6a5186573b2eb239e1a6410
Ben Haugen <ben1232jazz@hotmail.com>
2019-01-28 06:54:28 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,69 @@
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |people|
+    people.each do |k, v|
+      if v == "Winner"
+        full_name = people["name"]
+        return full_name.split(" ").first
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+   data.each do |season, info|
+    info.each do |people|
+      people.each do |key,value|
+        if value == occupation
+          return people["name"]
+        end
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0 
+  data.each do |season, info|
+    info.each do |people|
+    people.each do |key, value|
+    if value == hometown
+      counter = counter + 1
+        end
+      end
+    end
+  end
+  return counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, info|
+    info.each do |people|
+      people.each do |key,value|
+        if value == hometown
+          return people["occupation"]
+        end
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  age_array = []
+  data[season].each do |stuff|
+    stuff.each do |key, value|
+      if key == "age"
+        age_array << value.to_i 
+      end
+    end
+  end
+  sum = 0
+  age_array.each do |num|
+    sum += num
+  end
+  #binding.pry
+  avg_age = sum.to_f / age_array.length
+  avg_age.round
+  #binding.pry
 end
